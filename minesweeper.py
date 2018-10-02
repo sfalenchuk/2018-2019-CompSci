4373f6da1f073f980ed23ad85cf7a484c1e59b23
Sasha <sfalenchuk20@choate.edu>
2018-10-02 09:16:32 -0400

    minesweeper assignment

---
+++ minesweeper.py
@@ -0,0 +1,54 @@
+import random
+
+i = []
+s = []
+
+for x in range (8):
+	r = 0
+	i.append(r)
+
+
+for y in range (8):
+	p = random.randrange(0,1,1)
+	a.append(p)
+
+for y in range (8):
+	p = random.randrange(0,1,1)
+	b.append(p)
+
+for y in range (8):
+	p = random.randrange(0,1,1)
+	c.append(p)
+
+for y in range (8):
+	p = random.randrange(0,1,1)
+	d.append(p)
+
+for y in range (8):
+	p = random.randrange(0,1,1)
+	e.append(p)
+
+for y in range (8):
+	p = random.randrange(0,1,1)
+	f.append(p)
+
+for y in range (8):
+	p = random.randrange(0,1,1)
+	g.append(p)
+
+for y in range (8):
+	p = random.randrange(0,1,1)
+	h.append(p)
+
+
+
+choice = input("What number do you choose?")
+
+print (i)
+print (i)
+print (i)
+print (i)
+print (i)
+print (i)
+print (i)
+
