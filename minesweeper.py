81e3efe9b9dd1c1ece84f8fdcd282dabe429d3c8
Sasha <sfalenchuk20@choate.edu>
2018-10-08 09:35:04 -0400

    minesweeper project

--- minesweeper.py
+++ minesweeper.py
@@ -1,54 +1,65 @@
 import random
+import sys
 
-i = []
-s = []
+row = int(sys.argv[1])+2
+columns = int(sys.argv[2])+2
+bombs = int(sys.argv[3])
 
-for x in range (8):
-	r = 0
-	i.append(r)
-
-
-for y in range (8):
-	p = random.randrange(0,1,1)
-	a.append(p)
-
-for y in range (8):
-	p = random.randrange(0,1,1)
-	b.append(p)
-
-for y in range (8):
-	p = random.randrange(0,1,1)
-	c.append(p)
-
-for y in range (8):
-	p = random.randrange(0,1,1)
-	d.append(p)
-
-for y in range (8):
-	p = random.randrange(0,1,1)
-	e.append(p)
-
-for y in range (8):
-	p = random.randrange(0,1,1)
-	f.append(p)
-
-for y in range (8):
-	p = random.randrange(0,1,1)
-	g.append(p)
-
-for y in range (8):
-	p = random.randrange(0,1,1)
-	h.append(p)
+mylist = [[0]*columns for x in range (row)]
 
 
+for i in range (bombs):
+	down = random.randrange(1,row-1,1)
+	over = random.randrange(1,columns-1,1)
+	while mylist[down][over] is "*":
+		down = random.randrange(1,row-1,1)
+		over = random.randrange(1,columns-1,1)
+	mylist[down][over] = "*"
 
-choice = input("What number do you choose?")
+	if mylist[down][over-1] is not "*":
+		mylist[down][over-1] += 1
 
-print (i)
-print (i)
-print (i)
-print (i)
-print (i)
-print (i)
-print (i)
+	if mylist[down][over+1] is not "*":
+		mylist[down][over+1] += 1
+
+	if mylist[down-1][over+1] is not "*":
+		mylist[down-1][over+1] += 1
+
+	if mylist[down-1][over] is not "*":
+		mylist[down-1][over] += 1
+
+	if mylist[down-1][over-1] is not "*":
+		mylist[down-1][over-1] += 1
+
+	if mylist[down+1][over-1] is not "*":
+		mylist[down+1][over-1] += 1
+
+	if mylist[down+1][over] is not "*":
+		mylist[down+1][over] += 1
+
+	if mylist[down+1][over+1] is not "*":
+		mylist[down+1][over+1] += 1
+
+choiceoption = input("do you want to flag or choose?")
+
+if choiceoption == flag:
+	choiceflag = input("what space do you want to flag")
+
+if choiceoption == choose:
+	choicespace = input("what space do you choose?")
+
+for j in mylist[1:-1]:
+	print(*j[1:-1])
+
+for down  in range (0, len(mylist)):
+	for over in range (0, (len(mylist[0]))
+		
+if mylist[down][over] == "*"
+	print("Game Over!")
+
+
+
+
+
+
 
