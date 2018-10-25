dded938cfc78d449cf34d0641713d8b56f8abcbc
Sasha <sfalenchuk20@choate.edu>
2018-10-24 20:37:08 -0400

    projects

---
+++ mandelbrot2.py
@@ -0,0 +1,79 @@
+from PIL import Image
+import random
+
+xmin, xmax = -0.883333333333, -0.591666666666
+ymin, ymax = .1, .39166666666666
+
+amin, amax = -0.34166666666666, 0.1833333333333
+bmin, bmax = -1.09166666666666, -0.5666666666666
+
+cmin, cmax = -2.0, 1
+dmin, dmax = -1.5, 1.5
+
+imgx, imgy = 512, 512
+imga, imgb = 512, 512
+imgc, imgd = 512, 512
+
+maxIt = 256
+
+image = Image.new("RGB", (imgx,imgy))
+image2 = Image.new("RGB", (imga,imgb))
+image3 = Image.new("RGB", (imgc, imgd)) 
+
+m = complex(random.random() * 2.0 - 1.0, random.random() - 0.5)
+for y in range(imgy):
+	cy = y * (ymax-ymin) / (imgy-1) + ymin
+	for x in range(imgx):
+		cx = x * (xmax-xmin) / (imgx-1) + xmin
+		c = complex(cx, cy)
+		z = 0
+		for i in range(maxIt):
+			if abs(z) > 2.0:
+				break
+			z = z**2 + c
+
+		r = i
+		g = (i*50)%256
+		b = 256-i
+
+		image.putpixel((x,y),(r,g,b))
+
+for y in range(imgb):
+	cy = y * (bmax-bmin) / (imgb-1) + bmin
+	for x in range(imga):
+		cx = x * (amax-amin) / (imga-1) + amin
+		c = complex(cx, cy)
+		z = 0
+		for i in range(maxIt):
+			if abs(z) > 2.0:
+				break
+			z = z**2 + c
+
+		r = i
+		g = i%50
+		b = 100-i
+
+		image2.putpixel((x,y),(r,g,b))
+
+for y in range(imgd):
+	zy = y * (dmax - dmin) / (imgd - 1) + dmin
+	for x in range(imgc):
+		zx = x * (cmax - cmin) / (imgc - 1) + cmin
+		z = complex(zx, zy)
+		for i in range(maxIt):
+			if abs(z) > 2.0:
+				break
+			z = z**2 + m
+
+			r = 512-i
+			g = (i*100)%256
+			b = i-100
+
+
+			image3.putpixel((x,y),(b+0+r*65536))
+
+image.show()
+
+image2.show()
+ 
+image3.show()
--- minesweeper.py
+++ minesweeper.py
@@ -1,12 +1,41 @@
 import random
 import sys
 
+def print_mylist():
+	for j in mylist[1:-1]:
+		print(*j[1:-1])
+
+
+def print_game():
+	for v in game[1:-1]:
+		print(*v[1:-1])
+
+def reveal_zero(over,down):
+	zerolist = []
+	zerolist.append([over, down])
+	while len(zerolist) > 0:
+		over, down = zerolist.pop(0)
+		for o in range (-1,2):
+			for d in range (-1,2):
+				if mylist[over+o][down+d] == 0 and game[over+o][down+d] == 'x':
+					zerolist.append([over+o,down+d])
+				game[over+o][down+d] = mylist[over+o][down+d]
+
+#zeros in gutter
+#if you uncover a zero, and in uncovering you uncover another a zero next to it, no infinite loops.
+
+
+
+
 row = int(sys.argv[1])+2
 columns = int(sys.argv[2])+2
 bombs = int(sys.argv[3])
 
+game = [["x"]*columns for x in range (row)]
+
 mylist = [[0]*columns for x in range (row)]
 
+#print board
 
 for i in range (bombs):
 	down = random.randrange(1,row-1,1)
@@ -40,24 +69,65 @@ for i in range (bombs):
 	if mylist[down+1][over+1] is not "*":
 		mylist[down+1][over+1] += 1
 
-choiceoption = input("do you want to flag or choose?")
+mylist[0] = [1]*columns
+mylist[-1] = [1]*columns
 
-if choiceoption == flag:
-	choiceflag = input("what space do you want to flag")
+for y in range (row):
+	mylist[y][0] = 1
+	mylist[y][-1] = 1
 
-if choiceoption == choose:
-	choicespace = input("what space do you choose?")
+#print_mylist()
 
-for j in mylist[1:-1]:
-	print(*j[1:-1])
+print_game()
 
-for down  in range (0, len(mylist)):
-	for over in range (0, (len(mylist[0]))
+
+while True:
+
+	choiceoption = input("do you want to flag or choose?")
+
+	if choiceoption == "flag":
+		choiceflag = input("what space do you want to flag")
+		coords = choiceflag.split(",")
+		over = int(coords[0] )
+		down = int(coords[1] )
+		game[down][over] = "P"
+		# print_game()
+
+	elif choiceoption == "choose":
+		choicespace = input("what space do you choose?")
+		coords = choicespace.split(",")
+		over = int(coords[0] )
+		down = int(coords[1] )
+		game[down][over] = mylist[down][over]
+		# print_game()
+
+		if mylist[down][over]== 0:
+			
+			reveal_zero(over,down)
+
+			
+
+		elif mylist[down][over]  == "*":
+			print_mylist()
+			print("nice try, you lose.")
+			quit()
+
+		elif mylist[down][over] >= 1 or mylist[down][over] <= 9:
+			game[over][down] == mylist[over][down]
+
+
+	
+	print_game()
+
+
+
+
+
+# for down  in range (0, len(mylist)):
+# 	for over in range (0, (len(mylist[0]))
 		
-if mylist[down][over] == "*"
-	print("Game Over!")
-
-
+# if mylist[down][over] == "*"
+# 	print("Game Over!")
 
 
 
