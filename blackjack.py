c4a7d96663c1aaf3e2188479607f6f5eb00006b2
Sasha <sfalenchuk20@choate.edu>
2018-11-07 08:59:25 -0500

    blackjack

---
+++ blackjack.py
@@ -0,0 +1,13 @@
+import random
+from card import Card
+from deck import Deck
+from random import shuffle
+
+class Dealer:
+    def __init__(self, balance, hand):
+        self.name = name
+        self.balance = balance
+        self.status = status
+
+
+            
--- card.py
+++ card.py
@@ -1,38 +1,26 @@
 class Card:
+
 	def __init__(self, rank, suit):
-		self.suit = suit
 		self.rank = rank
+		self.suit = suit
 
 	def __str__(self):
-		status = ""
 		rank = self.rank
-			if self.rank = 1
-				rank = "Ace"
-			elif self.rank = 11
-				rank = "Jack"
-			elif self.rank = 12
-				rank = "Queen"
-			elif self.rank = 13
-				rank = "King"
+		if self.rank == 11:
+			rank = "Jack"
+		elif self.rank == 12:
+			rank = "Queen"
+		elif self.rank == 13:
+			rank = "King"
+		elif self.rank == 1:
+			rank = "Ace"
+		return str(rank)+" of "+self.suit
+
+	__repr__ = __str__
 
 
 
-		return self.rank +"of"+ self.suit
-
-c1 = Card(1,"Hearts")
-c2 = Card(6,"Hearts")
-c3 = Card(13,"Hearts")
-
-print(c1,c2,c3)
-
-# class Deck:
-# 	def __init__(self):
-# 		self.cards = []
-# 		for i in range(1, 14)
-# 			self.cards.append(Card(i,"Spades")
-# 			self.cards.append(Card(i,"Hearts")
-# 			self.cards.append(Card(i,"Clubs")
-# 			self.cards.append(Card(i,"Diamonds")
+		
 
 
 
