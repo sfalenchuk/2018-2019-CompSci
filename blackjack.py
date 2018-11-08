38a2add71ee2755bcb830c37e4b7af590681dcaa
Sasha <sfalenchuk20@choate.edu>
2018-11-08 10:03:30 -0500

    blackjack project

--- blackjack.py
+++ blackjack.py
@@ -4,10 +4,46 @@ from deck import Deck
 from random import shuffle
 
 class Dealer:
-    def __init__(self, balance, hand):
+    def __init__(self, balance):
         self.name = name
         self.balance = balance
         self.status = status
+
+class Dealer:
+    def __init__(self, balance):
+        self.name = name
+        self.balance = balance
+        self.status = status
+
+class Hand:
+    def __init__(self):
+        self.hand = []
+
+    def add_card(self, card):
+        self.hand.append(card)
+        return self.hand
+
+class DealerHand:
+    def __init__(self):
+        self.hand = []
+
+    def add_card(self, card):
+        self.hand.append(card)
+        return self.hand
+
+print("Hello, and welcome to Blackjack. Blackjack is an advanced gambling game in which it is your job to reach the number 21")
+
+print("You will start out with one card, and after that you will have a choice to hit or pass. If you hit, you will get another card, in an attempt to get closer to 21. But be careful, get over 21 and you will lose!")
+
+print("The game will continue until the players finish their turns, and then everyone will reveal their cards")
+
+choice = input("All players start with ten thousand dollars, ready to begin?")
+
+	if choice == "yes":
+
+	elif choice =="no":
+		print("Alright, read the rules again")
+
 
 
             
