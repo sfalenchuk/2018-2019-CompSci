5fc15ecdae9d14be6e74fd9e65e29c5d6ddd9986
Sasha <sfalenchuk20@choate.edu>
2019-01-16 22:35:33 -0500

    up

--- group_work/loop.py
+++ group_work/loop.py
@@ -1,3 +1,6 @@
+#Sasha Falenchuk
+#12/17/18
+
 ''' Instructions:
    Work with a partner to complete these tasks. Assume that all variables mentioned in the description are declared and initialized; however, feel free to use additional variable as necessary (please avoid extra variables, though; don't use them unless you must to store a required value or simplify your code.) Write your solution below the commented description.
 '''
